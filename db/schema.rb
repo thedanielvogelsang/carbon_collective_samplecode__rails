811e0f33a1f732f55457ee412b43d36a307a96be
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-03-15 16:18:36 -0600

    adds carbon_saved onto electric_bills for model-level calculations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315214909) do
+ActiveRecord::Schema.define(version: 20180315221731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20180315214909) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.float "price"
+    t.float "carbon_saved"
     t.index ["user_id"], name: "index_electric_bills_on_user_id"
   end
 
