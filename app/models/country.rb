64575088419da7a00f854ac546fa68c8b54784f4
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-03-20 13:53:57 -0600

    adds United States to check_name method in Country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -6,5 +6,6 @@ class Country < ApplicationRecord
 
   def check_name
     self.name == "USA" ? self.name = "United States of America" : nil
+    self.name == "United States" ? self.name = "United States of America" : nil
   end
 end
