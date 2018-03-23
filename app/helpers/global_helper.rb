c87d4f6568b505085f588bd61ee4b2c629248b73
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-03-23 05:48:01 -0600

    after upgrading node to v9.9.0; changed show and group_helpers to include new user_co2_helpers functions

--- app/helpers/global_helper.rb
+++ app/helpers/global_helper.rb
@@ -1,5 +1,5 @@
 module GlobalHelper
   def self.total_to_date
-    User.all.map{|u| u.total_co2_saved}.reduce(0){|s,n| s+n}
+    User.all.map{|u| u.total_carbon_savings_to_date}.reduce(0){|s,n| s+n}
   end
 end
