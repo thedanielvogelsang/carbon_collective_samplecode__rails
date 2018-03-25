9aeef038a6307734b893a4f5a044af911851fefe
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-03-25 15:30:12 -0600

    changes houses (and state for formal commit on behalf of address) to destroy dependent associations (other than users)

--- app/models/house.rb
+++ app/models/house.rb
@@ -4,6 +4,7 @@ class House < ApplicationRecord
   belongs_to :address
   validates_presence_of :total_sq_ft
   validates_presence_of :no_residents
+  
   validates :address_id, presence: true, uniqueness: true
 
   has_many :user_houses, :dependent => :destroy
