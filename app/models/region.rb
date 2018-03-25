cf4a8c7d814a831b443a4fa8c6f3d121341158ca
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-03-25 14:39:40 -0600

    creates same validations for name uniquness on region and city as neighborhood

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,7 +1,8 @@
 class Region < ApplicationRecord
   include RegionHelper
-  
-  validates :name, presence: true, uniqueness: true
+
+  validates_presence_of :name
+  validates_uniqueness_of :name, scope: :country_id
 
   belongs_to :country
   has_many :cities
