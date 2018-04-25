8b0b662acd4593b881556946ce9a44e33beaff86
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-04-25 15:28:16 -0600

    deletes update and destroy actions in api/v1/houses_controller

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -7,22 +7,6 @@ class Api::V1::HousesController < ApplicationController
     render json: House.find(params[:id])
   end
 
-  def update
-    house = House.find(params[:id])
-    house.update(safe_params)
-    if house.save
-      render json: house, status: 202
-    else
-      render file: "public/404"
-    end
-  end
-
-  def destroy
-    house = House.find(params[:id])
-    house.destroy
-    render json: house, status: 204
-  end
-
   private
     def safe_params
       params.require(:house).permit(:total_sq_ft, :no_residents, :address_id)
