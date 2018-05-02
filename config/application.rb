cfc6f061b757ec0a778c9609a10eb13117c9edeb
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-05-02 11:41:24 -0600

    users can be updated, adds a PUT cors option in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module CarbonCollective
     config.middleware.insert_before 0, Rack::Cors do
           allow do
             origins '*'
-            resource '*', :headers => :any, :methods => [:get, :post, :options]
+            resource '*', :headers => :any, :methods => [:get, :post, :options, :put]
           end
         end
     # Settings in config/environments/* take precedence over those specified here.
