846c47fff3efcf761cc947a09d748f2003e32eb1
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-05-09 22:53:42 -0600

    restructures user serializer -- deletes electricity savings from user and anticipates resource_controllers to have corresponding serializers; dashboard_data should use these endpoints to find totals, etc. for each Area

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,13 +2,9 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :first, :last, :email,
                   :avatar_url, :house_ids,
                   :total_carbon_savings_to_date,
-                  :global_collective_electricity_savings, :address,
+                  :global_collective_carbon_savings, :address,
                   :household, :neighborhood, :city, :region, :country,
-                  :household_total_savings,
-                  :neighborhood_total_savings,
-                  :city_total_savings,
-                  :region_total_savings,
-                  :country_total_savings
+
 
   def neighborhood
     [object.neighborhood.id, object.neighborhood] if object.neighborhood
@@ -27,11 +23,11 @@ class UserSerializer < ActiveModel::Serializer
     object.houses.map{|h| h.id} if object.houses.length > 0
   end
 
-  def total_electricity_savings_to_date
+  def total_carbon_savings_to_date
     object.total_electricity_savings.to_f.round(2).to_s + " kwhs"
   end
 
-  def global_collective_electricity_savings
+  def global_collective_carbon_savings
     Global.first.total_energy_saved.to_f.round(2).to_s + " kwhs"
   end
 
@@ -53,20 +49,5 @@ class UserSerializer < ActiveModel::Serializer
 
   def avatar_url
     object.url
-  end
-  def household_total_savings
-    object.household_total_savings.to_f.round(2).to_s if !object.houses.empty?
-  end
-  def neighborhood_total_savings
-    object.neighborhood_total_savings.to_f.round(2).to_s if !object.houses.empty?
-  end
-  def city_total_savings
-    object.city_total_savings.to_f.round(2).to_s if !object.houses.empty?
-  end
-  def region_total_savings
-    object.region_total_savings.to_f.round(2).to_s if !object.houses.empty?
-  end
-  def country_total_savings
-    object.country_total_savings.to_f.round(2).to_s if !object.houses.empty?
   end
 end
