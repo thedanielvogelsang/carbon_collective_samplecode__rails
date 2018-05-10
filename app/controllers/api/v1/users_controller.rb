2e9c2ad5568a3d0d03eeeca8cad2e438bb7708ef
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-05-10 00:23:59 -0600

    updates house_helper to include water; user_controller to include endpoint for resources?resource=water with UserWaterSerializer; api endpoint now showing successfully

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,7 +14,7 @@ class Api::V1::UsersController < ApplicationController
   def resources
     if User.exists?(params[:id])
       render json: User.find(params[:id]), serializer: UserElectricitySerializer if params[:resource] == 'electricity'
-      # render json: User.find(params[:id]), serializer: UserWaterSerializer if params[:resource] == 'water'
+      render json: User.find(params[:id]), serializer: UserWaterSerializer if params[:resource] == 'water'
       # render json: User.find(params[:id]), serializer: UserGasSerializer if params[:resource] == 'gas'
     else
       render json: {error: "User does not exist"}, status: 404
