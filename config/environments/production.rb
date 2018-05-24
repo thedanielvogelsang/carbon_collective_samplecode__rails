c34922c91b6010372b7dae0c497e47f163a96186
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-05-24 10:33:51 -0600

    attempting to push to heroku with new mailers configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,20 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "carbon_collective_#{Rails.env}"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "https://carboncollective.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address        => "smtp.gmail.com",
+    :port           => 587,
+    :domain         => "gmail.com",
+    :user_name      => ENV['GMAIL_ACCT'],
+    :password       => ENV['GMAIL_PASSWORD'],
+    :authentication => :plain
+  }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
