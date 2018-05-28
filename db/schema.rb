f09f3334c37bda768c1dcac3f747d5e623f8b84c
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-05-27 20:41:04 -0600

    creates apartment boolean that defaults to false for apartment/house toggle on HOUSE model; updates schema; adds :apartment to update and create house actions (diff controllers) in safe_params

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180523200523) do
+ActiveRecord::Schema.define(version: 20180528023236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -204,6 +204,7 @@ ActiveRecord::Schema.define(version: 20180523200523) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "address_id"
+    t.boolean "apartment"
     t.index ["address_id"], name: "index_houses_on_address_id"
   end
 
