5fe8198b94e6aba0a0125c080bb62556a574da1f
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-06-14 15:17:42 -0600

    updates water and electricity questions to calcualte completion percentage as well

--- app/models/user_water_question.rb
+++ app/models/user_water_question.rb
@@ -4,4 +4,17 @@ class UserWaterQuestion < ApplicationRecord
 
   validates_uniqueness_of :user_id, scope: :house_id
   validates_uniqueness_of :house_id, scope: :user_id
+
+  before_save :update_completion
+
+  def update_completion
+    ct = 0;
+    self.quest1 ? ct += 1 : nil
+    self.quest2 ? ct += 1 : nil
+    self.quest3 ? ct += 1 : nil
+    self.quest5 ? ct += 1 : nil
+    self.a_count = ct
+    self.completion_percentage = self.a_count.fdiv(self.q_count) * 100
+    self.completion_percentage.to_i == 100 ? self.completed = true : nil
+  end
 end
