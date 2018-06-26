cfce030a5cb263dfc1d6c95337dffed4ef7900f8
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-06-25 20:29:39 -0600

    attempts to fix co2 helper math

--- app/helpers/co2_helper.rb
+++ app/helpers/co2_helper.rb
@@ -5,12 +5,12 @@ module Co2Helper
   end
   def kwhs_to_carbon(elect_in_kwhs)
     #kwhs to metric tons to lbs
-    elect_in_kwhs * 0.000744 / 0.0004536
+    elect_in_kwhs * 1.222
   end
 
   def combine_average_use(kwh_avg, therm_avg)
     kwh_avg = 0 if kwh_avg.to_f.nan?
     therm_avg = 0 if therm_avg.to_f.nan?
-    return ((kwh_avg * 0.000744) + (therm_avg * 0.0053))
+    return ((kwh_avg * 1.222) + (therm_avg * 0.0053 / 0.0004536))
   end
 end
