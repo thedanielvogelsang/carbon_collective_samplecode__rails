4b037e102247b4dae4745f26a20c59302ac983dc
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-07-02 13:09:46 -0600

    minor update on therms to co2 conversion

--- app/helpers/co2_helper.rb
+++ app/helpers/co2_helper.rb
@@ -1,7 +1,7 @@
 module Co2Helper
   def therms_to_carbon(gas_in_therms)
     #therms to metric tons to lbs
-    gas_in_therms * 0.0053 / 0.0004536
+    gas_in_therms * 0.00548 / 0.0004536
   end
   def kwhs_to_carbon(elect_in_kwhs)
     #kwhs to metric tons to lbs
