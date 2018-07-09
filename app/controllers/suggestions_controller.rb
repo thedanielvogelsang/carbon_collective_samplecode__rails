acc94257fba8911e12828fb0f91c37f1ef7ea9f2
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-07-09 17:54:30 -0600

    adds new action in sugggestions_controller for retaining user regional data for users who cant sign up; adds region_expansion route (which apparently wasnt being used despite its existence for some time) for proper emailing to carboncollective.devops; changes the styling and order in the invite.html.erb email

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -14,6 +14,13 @@ class SuggestionsController < ApplicationController
     render json: {:success => "Message sent"}, status: 202
   end
 
+  def region_data
+    user = User.find(params[:user][:id])
+    mail = SuggestionMailer.send_geographical_data
+    mail.deliver_now
+    render json: {:success => "regions stored"}, status: 202
+  end
+
   def send_bug
     user = User.find(params[:user][:id])
     mail = SuggestionMailer.send_bug_fix_request(user, params[:email_body])
