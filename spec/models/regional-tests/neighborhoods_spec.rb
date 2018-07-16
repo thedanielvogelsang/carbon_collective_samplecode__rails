6573c21f293e9edab82195d624f4064fd7842def
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-07-15 22:44:03 -0600

    completes neighborhoods_spec for basic bill math confirmation for all 3 types of resources; to be franchised across other Area models

--- spec/models/regional-tests/neighborhoods_spec.rb
+++ spec/models/regional-tests/neighborhoods_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'neighborhood consumption averages' do
   before :each do
     avg = 12071.fdiv(30)
-    @country = Country.create(name: "Country-land", avg_daily_electricity_consumed_per_capita: avg)
+    wavg = 409466.6
+    gavg = 10.88
+    @country = Country.create(name: "Country-land", avg_daily_electricity_consumed_per_capita: avg, avg_daily_water_consumed_per_capita: wavg, avg_daily_gas_consumed_per_capita: gavg)
     @region = Region.create(name: "Region-land", country_id: @country.id)
     @city = City.create(name: "City", region_id: @region.id)
     @county = County.create(name: "County-within", region_id: @region.id)
@@ -142,9 +144,9 @@ RSpec.describe 'neighborhood consumption averages' do
       user = User.first
       neighborhood = Neighborhood.first
       neighborhood2 = Neighborhood.second
-      n1_avg = neighborhood.avg_daily_electricity_consumed_per_user
-      n2_avg = neighborhood2.avg_daily_electricity_consumed_per_user
-      u_avg = user.avg_daily_electricity_consumption
+      n1_avg = neighborhood.avg_daily_water_consumed_per_user
+      n2_avg = neighborhood2.avg_daily_water_consumed_per_user
+      u_avg = user.avg_daily_water_consumption
       expect(n1_avg).to eq(0)
       expect(n2_avg).to eq(0)
       expect(u_avg.nan?).to be true
@@ -154,16 +156,16 @@ RSpec.describe 'neighborhood consumption averages' do
       house = House.first
       neighborhood = Neighborhood.first
       expect(user.neighborhood).to eq(neighborhood)
-      kwhs = 1400
+      gallons = 14000
       price = rand(1..100)
-      ElectricBill.create(start_date: @start_date1, end_date: @end_date1, total_kwhs: kwhs, price: price, house_id: house.id, no_residents: 2, user_id: user.id)
+      WaterBill.create(start_date: @start_date1, end_date: @end_date1, total_gallons: gallons, price: price, house_id: house.id, no_residents: 2, user_id: user.id)
       user = User.first
-      n1_avg = neighborhood.avg_daily_electricity_consumed_per_user
-      u_avg = user.avg_daily_electricity_consumption
+      n1_avg = neighborhood.avg_daily_water_consumed_per_user
+      u_avg = user.avg_daily_water_consumption
       expect(n1_avg).to eq(0)
-      expect(u_avg.to_f.round(2)).to eq(23.33)
-      neighborhood.update_daily_avg_electricity_consumption
-      n1_avg = neighborhood.avg_daily_electricity_consumed_per_user
+      expect(u_avg.to_f.round(2)).to eq(233.33)
+      neighborhood.update_daily_avg_water_consumption
+      n1_avg = neighborhood.avg_daily_water_consumed_per_user
       expect(n1_avg).to eq(u_avg)
     end
     it 'neighborhood avgs reflect accurate change upon new bill entry' do
@@ -172,27 +174,27 @@ RSpec.describe 'neighborhood consumption averages' do
       neighborhood = Neighborhood.first
       expect(user.houses.first).to eq(house)
       expect(user.neighborhood).to eq(neighborhood)
-      kwhs = 1400
-      kwhs2 = 2800
+      gallons = 14000
+      gallons2 = 28000
       price = rand(1..100)
-      ElectricBill.create(start_date: @start_date1, end_date: @end_date1, total_kwhs: kwhs, price: price, house_id: house.id, no_residents: 2, user_id: user.id)
-      neighborhood.update_daily_avg_electricity_consumption
-      n1_avg1 = neighborhood.avg_daily_electricity_consumed_per_user
-      expect(n1_avg1.to_f.round(2)).to eq(23.33)
+      WaterBill.create(start_date: @start_date1, end_date: @end_date1, total_gallons: gallons, price: price, house_id: house.id, no_residents: 2, user_id: user.id)
+      neighborhood.update_daily_avg_water_consumption
+      n1_avg1 = neighborhood.avg_daily_water_consumed_per_user
+      expect(n1_avg1.to_f.round(2)).to eq(233.33)
       #-- add second bill --#
-      ElectricBill.create(start_date: @start_date2, end_date: @end_date2, total_kwhs: kwhs2, price: price, house_id: house.id, no_residents: 2, user_id: user.id)
+      WaterBill.create(start_date: @start_date2, end_date: @end_date2, total_gallons: gallons2, price: price, house_id: house.id, no_residents: 2, user_id: user.id)
       user = User.first
-      u_avg = user.avg_daily_electricity_consumption
-      n1_new_avg = neighborhood.avg_daily_electricity_consumed_per_user
-      expect(u_avg.to_f.round(2)).to eq(35.0)
-      expect(n1_new_avg.to_f.round(2)).to eq(23.33)
+      u_avg = user.avg_daily_water_consumption
+      n1_new_avg = neighborhood.avg_daily_water_consumed_per_user
+      expect(u_avg.to_f.round(2)).to eq(350.0)
+      expect(n1_new_avg.to_f.round(2)).to eq(233.33)
       expect(n1_new_avg).to eq(n1_avg1)
       #user consumed twice as much second month
-      expect(kwhs * 2).to eq(kwhs2)
+      expect(gallons * 2).to eq(gallons2)
       #--- updates neighborhood ---- #
       neighborhood = Neighborhood.first
-      neighborhood.update_daily_avg_electricity_consumption
-      n1_avg2 = neighborhood.avg_daily_electricity_consumed_per_user
+      neighborhood.update_daily_avg_water_consumption
+      n1_avg2 = neighborhood.avg_daily_water_consumed_per_user
       #new average is twice as
       expect(n1_avg2).to eq(u_avg)
     end
@@ -203,30 +205,115 @@ RSpec.describe 'neighborhood consumption averages' do
       expect(User.first.neighborhood).to eq(User.second.neighborhood)
       expect(User.second.neighborhood).to_not eq(User.third.neighborhood)
       #-- add bill to second house--#
-      kwhs = 1400
+      gallons = 14000
       price = rand(1..100)
-      bill = ElectricBill.create(start_date: @start_date1, end_date: @end_date1, total_kwhs: kwhs, price: price, house_id: House.first.id, no_residents: 2, user_id: User.first.id)
+      WaterBill.create(start_date: @start_date1, end_date: @end_date1, total_gallons: gallons, price: price, house_id: House.first.id, no_residents: 2, user_id: User.first.id)
       #-- add bill to second house--#
-      kwhs2 = 2800
+      gallons2 = 28000
       price = rand(1..100)
-      bill = ElectricBill.create(start_date: @start_date2, end_date: @end_date2, total_kwhs: kwhs2, price: price, house_id: House.second.id, no_residents: 2, user_id: User.second.id)
+      WaterBill.create(start_date: @start_date2, end_date: @end_date2, total_gallons: gallons2, price: price, house_id: House.second.id, no_residents: 2, user_id: User.second.id)
       user = User.first
       user2 = User.second
-      u_avg = user.avg_daily_electricity_consumption
-      u2_avg = user2.avg_daily_electricity_consumption
+      u_avg = user.avg_daily_water_consumption
+      u2_avg = user2.avg_daily_water_consumption
       #first average is twice as large as first persons average
-      expect(u_avg.to_f.round(1)).to eq(23.3)
-      expect(u2_avg.to_f.round(1)).to eq(46.7)
+      expect(u_avg.to_f.round(1)).to eq(233.3)
+      expect(u2_avg.to_f.round(1)).to eq(466.7)
       #--- updates neighborhood ---- #
       neighborhood = User.first.neighborhood
-      neighborhood.update_daily_avg_electricity_consumption
-      n1_avg = neighborhood.avg_daily_electricity_consumed_per_user
+      neighborhood.update_daily_avg_water_consumption
+      n1_avg = neighborhood.avg_daily_water_consumed_per_user
       #first average is twice as large as first persons average
-      expect(n1_avg.to_f.round(1)).to eq(35.0)
+      expect(n1_avg.to_f.round(1)).to eq(350.0)
     end
   end
-  xcontext "Heat Bills / 1 & 2 users affect (same) neighborhood" do
-
+  context "Heat Bills / 1 & 2 users affect (same) neighborhood" do
+    it 'neighborhoods do not innately reflect their users averages' do
+      user = User.first
+      neighborhood = Neighborhood.first
+      neighborhood2 = Neighborhood.second
+      n1_avg = neighborhood.avg_daily_gas_consumed_per_user
+      n2_avg = neighborhood2.avg_daily_gas_consumed_per_user
+      u_avg = user.avg_daily_gas_consumption
+      expect(n1_avg).to eq(0)
+      expect(n2_avg).to eq(0)
+      expect(u_avg.nan?).to be true
+    end
+    it 'neighborhoods reflect single bill upon update' do
+      user = User.first
+      house = House.first
+      neighborhood = Neighborhood.first
+      expect(user.neighborhood).to eq(neighborhood)
+      therms = 300
+      price = rand(1..100)
+      HeatBill.create(start_date: @start_date1, end_date: @end_date1, total_therms: therms, price: price, house_id: house.id, no_residents: 2, user_id: user.id)
+      user = User.first
+      n1_avg = neighborhood.avg_daily_gas_consumed_per_user
+      u_avg = user.avg_daily_gas_consumption
+      expect(n1_avg).to eq(0)
+      expect(u_avg.to_f.round(2)).to eq(5.0)
+      neighborhood.update_daily_avg_gas_consumption
+      n1_avg = neighborhood.avg_daily_gas_consumed_per_user
+      expect(n1_avg).to eq(u_avg)
+    end
+    it 'neighborhood avgs reflect accurate change upon new bill entry' do
+      user = User.first
+      house = House.first
+      neighborhood = Neighborhood.first
+      expect(user.houses.first).to eq(house)
+      expect(user.neighborhood).to eq(neighborhood)
+      therms = 300
+      therms2 = 600
+      price = rand(1..100)
+      HeatBill.create(start_date: @start_date1, end_date: @end_date1, total_therms: therms, price: price, house_id: house.id, no_residents: 2, user_id: user.id)
+      neighborhood.update_daily_avg_gas_consumption
+      n1_avg1 = neighborhood.avg_daily_gas_consumed_per_user
+      expect(n1_avg1.to_f.round(2)).to eq(5.0)
+      #-- add second bill --#
+      HeatBill.create(start_date: @start_date2, end_date: @end_date2, total_therms: therms2, price: price, house_id: house.id, no_residents: 2, user_id: user.id)
+      user = User.first
+      u_avg = user.avg_daily_gas_consumption
+      n1_new_avg = neighborhood.avg_daily_gas_consumed_per_user
+      expect(u_avg.to_f.round(2)).to eq(7.50)
+      expect(n1_new_avg.to_f.round(2)).to eq(5.0)
+      expect(n1_new_avg).to eq(n1_avg1)
+      #user consumed twice as much second month
+      expect(therms * 2).to eq(therms2)
+      #--- updates neighborhood ---- #
+      neighborhood = Neighborhood.first
+      neighborhood.update_daily_avg_gas_consumption
+      n1_avg2 = neighborhood.avg_daily_gas_consumed_per_user
+      #new average is twice as
+      expect(n1_avg2).to eq(u_avg)
+    end
+    it 'neighborhood avgs reflect accurate mean between two users' do
+      #add users to separate houses within same neighborhood
+      #check same neighborhood
+      User.first.clear_totals
+      expect(User.first.neighborhood).to eq(User.second.neighborhood)
+      expect(User.second.neighborhood).to_not eq(User.third.neighborhood)
+      #-- add bill to second house--#
+      therms = 300
+      price = rand(1..100)
+      HeatBill.create(start_date: @start_date1, end_date: @end_date1, total_therms: therms, price: price, house_id: House.first.id, no_residents: 2, user_id: User.first.id)
+      #-- add bill to second house--#
+      therms2 = 600
+      price = rand(1..100)
+      HeatBill.create(start_date: @start_date2, end_date: @end_date2, total_therms: therms2, price: price, house_id: House.second.id, no_residents: 2, user_id: User.second.id)
+      user = User.first
+      user2 = User.second
+      u_avg = user.avg_daily_gas_consumption
+      u2_avg = user2.avg_daily_gas_consumption
+      #first average is twice as large as first persons average
+      expect(u_avg.to_f.round(1)).to eq(5.0)
+      expect(u2_avg.to_f.round(1)).to eq(10.0)
+      #--- updates neighborhood ---- #
+      neighborhood = User.first.neighborhood
+      neighborhood.update_daily_avg_gas_consumption
+      n1_avg = neighborhood.avg_daily_gas_consumed_per_user
+      #first average is twice as large as first persons average
+      expect(n1_avg.to_f.round(1)).to eq(7.50)
+    end
   end
   context "two people within area affects entire score" do
 
