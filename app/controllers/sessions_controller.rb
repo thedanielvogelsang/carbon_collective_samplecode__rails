2174b16d0c6c776a5e1bc971565a68e2c1012895
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-07-17 12:43:42 -0600

    sessions changes for login speed. took out the if clause for facebook omniauth (deprecated)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,34 +9,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # Not being used currently; facebook login
-    if request.env['omniauth.auth']
-      user = User.create_with_omniauth(request.env['omniauth.auth'])
-      session[:user_id] = user.id
-      redirect_to new_user_path({:user => {uid: user.uid}}) if user.addresses.empty?
-      redirect_to user_path(user.id) if !user.addresses.empty?
-    # regular app functions for new_user creation
-    else
-      user = User.find_by(email: safe_params[:email])
-      respond_to do |format|
-        if user && user.authenticate(safe_params[:password])
-          if user.email_confirmed
-            user.update_login
-            user.calc_avg
-            session[:user_id] = user.id
-            format.json {render :json => user}
-          else
-            error = "Email Not Confirmed! Please use the link sent to your email to continue..."
-            format.json {render :json => {:errors => error}, :status => 401 }
-          end
-        elsif user && !user.authenticate(safe_params[:password])
-          error = 'Password/Email did not match. Please try again. Remember both are case sensitive!'
-          format.json {render :json => {:errors => error}, :status => 401 }
-        else
-          error = 'Email not found. Email/Password are case-sensitive. Please try again. If you havent made a CarbonCollective account yet, remember youll need an invite!'
-          format.json {render :json => {:errors => error}, :status => 401 }
-        end
+    user = User.find_by(email: safe_params[:email])
+    if user && user.authenticate(safe_params[:password])
+      if user.email_confirmed
+        user.update_login
+        user.calc_avg
+        session[:user_id] = user.id
+        render :json => user
+      else
+        error = "Email Not Confirmed! Please use the link sent to your email to continue..."
+        render :json => {:errors => error}, :status => 401
       end
+    elsif user && !user.authenticate(safe_params[:password])
+      error = 'Password/Email did not match. Please try again. Remember both are case sensitive!'
+      render :json => {:errors => error}, :status => 401
+    else
+      error = 'Email not found. Email/Password are case-sensitive. Please try again. If you havent made a CarbonCollective account yet, remember youll need an invite!'
+      render :json => {:errors => error}, :status => 401
     end
   end
 
