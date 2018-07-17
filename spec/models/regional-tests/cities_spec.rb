525f5cfb8d9214e74f930408ea265bfd9ecea35f
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-07-16 20:28:44 -0600

    counties up to date with cities/neighborhoods on regional simple bill entry tests

--- spec/models/regional-tests/cities_spec.rb
+++ spec/models/regional-tests/cities_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'neighborhood consumption averages' do
+RSpec.describe 'city consumption averages' do
   before :each do
     avg = 12071.fdiv(30)
     wavg = 409466.6
