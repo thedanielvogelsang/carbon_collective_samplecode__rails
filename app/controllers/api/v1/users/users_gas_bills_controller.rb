b48836c54e07732a6b1c550c8036e42f31dec8b8
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-10-03 14:26:56 -0600

    fixes water_ and gas_ controllers for worker invocation

--- app/controllers/api/v1/users/users_gas_bills_controller.rb
+++ app/controllers/api/v1/users/users_gas_bills_controller.rb
@@ -16,11 +16,12 @@ class Api::V1::Users::UsersGasBillsController < ApplicationController
     bill = HeatBill.new(safe_params)
     bill.user_id = User.friendly.find(params[:user_id]).id
     if bill.save
-      puts 'BILL CREATED! STARTING JOB:'
+      puts 'BILL CREATED! STARTING WORKER:'
       AverageCalculatorJob.perform_async
       render json: bill, status: 201
     else
       error = bill.errors.messages.first[1][0]
+      puts error
       render :json => {errors: error}, status: 401
     end
   end
