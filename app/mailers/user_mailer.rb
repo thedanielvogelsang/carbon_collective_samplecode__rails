233fadd898e4ab5ad2de67ca31ef2246fd79c481
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-10-22 17:13:17 -0600

    experimental puts for message on invite

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,6 +8,8 @@ class UserMailer < ApplicationMailer
     @user = user
     @new = new_user
     @message = message
+    puts message == ''
+    puts message 
     mail(:to => @new.email,
          :from => @user.email,
          :subject => "Carbon Collective Invite from #{@user.first}"
