75f0a5174f6e45162bf1544077de68c1a72c9e06
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-10-25 00:22:16 -0600

    creates check_invite_job for destroy users who havent logged in yet

--- app/jobs/check_invite_job.rb
+++ app/jobs/check_invite_job.rb
@@ -2,9 +2,9 @@ class CheckInviteJob < ApplicationJob
   queue_as :default
 
   def perform(user)
-    if(user.user_invites.order(:created_at).last.created_at > DateTime.now - 10)
+    if(user.user_invites.order(:created_at).last.created_at >= DateTime.now - 10)
       user.destroy
-      
+    end
     # Do something later
   end
 end
--- app/models/user_invite.rb
+++ app/models/user_invite.rb
@@ -5,7 +5,13 @@ class UserInvite < ApplicationRecord
   validates_presence_of :invite_id,
                         :user_id
 
+  after_create :call_10_day_check
+
   def invited
     User.find(self.invite_id)
+  end
+
+  def call_10_day_check
+    CheckInviteJob.set(wait: 10.days).perform_later(User.find(self.invite_id))
   end
 end
