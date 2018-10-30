75f8991afbc70df848e73439e5c329f803592c93
thedanielvogelsang <dr.vogelsang26@gmail.com>
2018-10-30 11:10:28 -0600

    datetime vs time change differential on controller level

--- app/controllers/api/v1/users/users_water_bills_controller.rb
+++ app/controllers/api/v1/users/users_water_bills_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::Users::UsersWaterBillsController < ApplicationController
       render json: WaterBill.joins(:house)
                     .where(:houses => {id: house.id})
                     .order(end_date: :desc)
-                    .select{|b| b.start_date >= (uh.move_in_date - 1)}, each_serializer: WaterBillSerializer
+                    .select{|b| b.start_date >= (uh.move_in_date - (60*60*24))}, each_serializer: WaterBillSerializer
     end
   end
 
